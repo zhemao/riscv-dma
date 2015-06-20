275077dd2f19a3eb5a065050ced878b2d3dc225a
Howard Mao <zhehao.mao@gmail.com>
2015-06-19 18:32:12 -0700

    get rid of non-accelerated copy-beat

--- tests/dma-test.c
+++ tests/dma-test.c
@@ -11,9 +11,6 @@ int twobeat_src[ARR_SIZE] = {
 };
 int twobeat_dst[ARR_SIZE];
 
-#define COPY_ACCEL
-
-#ifdef COPY_ACCEL
 static inline void copy_beat(int *src, int *dst, int nbytes) {
 	asm volatile ("fence");
 	asm volatile ("custom0 0, %[src], %[dst], 0" : :
@@ -21,15 +18,6 @@ static inline void copy_beat(int *src, int *dst, int nbytes) {
 	asm volatile ("custom0 0, %[nbytes], 0, 1" : : [nbytes] "r" (nbytes));
 	asm volatile ("fence");
 }
-#else
-static inline void copy_beat(int *src, int *dst, int nbytes) {
-	int nwords = nbytes / sizeof(int);
-	int i;
-
-	for (i = 0; i < nwords; i++)
-		dst[i] = src[i];
-}
-#endif
 
 int main(void)
 {
