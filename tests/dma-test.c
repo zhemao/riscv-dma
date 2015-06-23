3fec2f7ad8d6c63ad3acc25a1683288f02909262
Howard Mao <zhehao.mao@gmail.com>
2015-06-23 12:10:39 -0700

    rename src and dst appropriately and don't rely on dst being zeroed initially

--- tests/dma-test.c
+++ tests/dma-test.c
@@ -3,13 +3,13 @@
 #define DST_OFF  2
 #define ARR_SIZE 32
 
-int twobeat_src[ARR_SIZE] = {
+int twoblock_src[ARR_SIZE] = {
 	0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
 	0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
 	0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
 	0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F
 };
-int twobeat_dst[ARR_SIZE];
+int twoblock_dst[ARR_SIZE];
 
 static inline void copy_beat(int *src, int *dst, int nbytes) {
 	asm volatile ("fence");
@@ -21,15 +21,18 @@ static inline void copy_beat(int *src, int *dst, int nbytes) {
 
 int main(void)
 {
-	int *src = twobeat_src + SRC_OFF;
-	int *dst = twobeat_dst + DST_OFF;
+	int *src = twoblock_src + SRC_OFF;
+	int *dst = twoblock_dst + DST_OFF;
 	int wrong = 0;
 	int i;
+
+	for (i = 0; i < ARR_SIZE; i++)
+		twoblock_dst[i] = 0;
 
 	copy_beat(src, dst, COPY_SIZE * sizeof(int));
 
 	for (i = 0; i < DST_OFF; i++) {
-		if (twobeat_dst[i] != 0)
+		if (twoblock_dst[i] != 0)
 			wrong = 1;
 	}
 
@@ -39,7 +42,7 @@ int main(void)
 	}
 
 	for (i = DST_OFF + COPY_SIZE; i < ARR_SIZE; i++) {
-		if (twobeat_dst[i] != 0)
+		if (twoblock_dst[i] != 0)
 			wrong = 1;
 	}
 
